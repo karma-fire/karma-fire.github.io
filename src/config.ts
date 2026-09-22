d5d7a96f46a0289f51ac32f6bd2c60e7fd0289e9
Peyton <karmafire259@gmail.com>
2026-09-23 02:11:36 +0800

    chore: change bio

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Karma Fire",
+	bio: "谦虚地坚持",
 	links: [
 		{
 			name: "Twitter",
