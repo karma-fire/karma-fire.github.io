b64606c0f4d250585966dbbd8249e64a55782862
Peyton <karmafire259@gmail.com>
2026-09-25 22:17:34 +0800

    add: my avatar

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/MyDarling.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Karma Fire",
 	bio: "谦虚地坚持",
 	links: [
